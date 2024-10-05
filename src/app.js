f7b9ead822ffdc882a84cc4cf1684d847e3df530
karunakar14 <karunakarreddy42435@gmail.com>
2024-10-05 09:23:22 +0530

    updated ap.js

--- src/app.js
+++ src/app.js
@@ -1,22 +1,10 @@
 import React from "react";
-import Tic from "./counter and tic-tac-toe/tic";
 import Counter from "./class/counter";
-import DigitalClock from "./class/digitalClock";
-import Search from "./class/searchimage";
-import Birthday from "./birthday";
-import Weather from "./weather";
-//import "../src/tic-tac-toe.css"
-import "../src/counter and tic-tac-toe/index.css"
-
+import "../src/index.css"
  let App=()=>{
     return(
         <>
-       {/* <Tic/> */}
        <Counter/>
-       {/* <DigitalClock/>
-       <Search/> */}
-       {/* <Birthday/> */}
-       {/* <Weather/> */}
         </>
     )
  }
